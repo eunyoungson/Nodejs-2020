df03b780f9e6f19f6d949ac7bbd1b616ecfb988a
Eunyoung Son <lp0170@gmail.com>
2020-09-29 10:56:17 +0900

    app

--- 10_http모듈/app.js
+++ 10_http모듈/app.js
@@ -3,33 +3,41 @@ const url = require('url');
 const qs = require('querystring');
 const fs = require('fs');
 const view = require('./view/index');
-//const view2 = require('./view/text')
+const template =require('./view/templete')
 
  http.createServer((req,res) =>{  
     let pathname = url.parse(req.url).pathname;
+    let query = url.parse(req.url, true).query;
+    console.log(pathname,query.id);
     switch(pathname) {
         case '/':
-            fs.readdir('data',function(error, filelist){
-                let list ='';
-                for(let file of filelist) {
-                    let filename =file.substring(0,file.length-4);
-                    list += `<li><a href="/?id=${filename}">${filename}</a></li>`
-                }
-                let html = view.index(list);
-                res.end(html);
-            });
-            fs.readFile('view/index.html', 'utf8',(error,html) => {
-                res.end(html);
-            });
-               // let html2 = view2.text(text);
-            /* fs.readFile('view/text.html', 'utf8',(error,html) => {
-                res.end(html2);
-            }); */
+            if (query.id === undefined) {   
+                fs.readdir('data',function(error, filelist){                    
+                    let list =template.listGen(filelist);
+                    let content = template.HOME_CONTENTS;
+                    let control = template.buttonGen();
+                    let html = view.index('web 기술', list, content,control);                  
+                    res.end(html);
+                });
+            } else {
+
+                fs.readdir('data',function(error, filelist){                    
+                    let list =template.listGen(filelist);
+                    let title = query.id ;     
+                    let control = template.buttonGen(title);               
+                    let filename ='data/' + title + '.txt';
+                    fs.readFile(filename,'utf8',(error,buffer) => {
+                        let html = view.index(title,list,buffer,control);
+                        res.end(html);
+                    })
+                    
+                });
+            }           
             break;
 
         default:
-            res.writeHead(404, {'content-Type' : 'text/html'});
-            res.end(html );
+            res.writeHead(404);
+            res.end();
     
     }
  }).listen(3000,() =>{
---
+++ 10_http모듈/app_초기화면.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const url = require('url');
+const qs = require('querystring');
+const fs = require('fs');
+const view = require('./view/index');
+//const view2 = require('./view/text')
+
+ http.createServer((req,res) =>{  
+    let pathname = url.parse(req.url).pathname;
+    switch(pathname) {
+        case '/':
+            fs.readdir('data',function(error, filelist){
+                let list ='';
+                for(let file of filelist) {
+                    let filename =file.substring(0,file.length-4);
+                    list += `<li><a href="/?id=${filename}">${filename}</a></li>`
+                }
+                let html = view.index(list);
+                res.end(html);
+            });
+            fs.readFile('view/index.html', 'utf8',(error,html) => {
+                res.end(html);
+            });
+               // let html2 = view2.text(text);
+            /* fs.readFile('view/text.html', 'utf8',(error,html) => {
+                res.end(html2);
+            }); */
+            break;
+
+        default:
+            res.writeHead(404, {'content-Type' : 'text/html'});
+            res.end(html );
+    
+    }
+ }).listen(3000,() =>{
+     console.log('server running at http://localhost:3000');
+ });
